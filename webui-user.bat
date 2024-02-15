11a276140c44bca3b541a3e52eb9ae415382fb16
Tatalebuj <153492149+Tatalebuj@users.noreply.github.com>
2024-02-15 19:33:04 +0200

    Update webui-user.bat
    Added in some commandline args.

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--use-directml --skip-torch-cuda-test --deepdanbooru --api --gradio-img2img-tool color-sketch
 
 call webui.bat
